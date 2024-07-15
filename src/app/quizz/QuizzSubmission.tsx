1220b6e65dc6ee63a0f33b33f2438ca62b12341b
Guy Ezra <guyez1@ac.sce.ac.il>
2024-07-16 00:48:57 +0300

    full quiz operation with 100% score speacial surprise

--- src/app/quizz/QuizzSubmission.tsx
+++ src/app/quizz/QuizzSubmission.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useEffect } from 'react';
+import React from 'react';
 import Bar from '@/components/Bar';
-
+import Image from 'next/image';
+import { useReward } from "react-rewards";
 
 type Props = {
   scorePercentage: number, 
@@ -10,11 +12,28 @@ type Props = {
 
 const QuizzSubmission = (props: Props) => {
   const { scorePercentage, score, totalQuestions } = props;
+  const { reward } = useReward('rewardId', 'confetti');
+
+  useEffect(() => {
+    if(scorePercentage === 100) {
+      reward();
+    }
+  }, [scorePercentage, reward])
+
   return (
     <div className="flex flex-col flex-1">
       <main className="py-11 flex flex-col gap-4 items-center flex-1 mt-24">
         <h2 className="text-3xl font-bold">Quizz Complete!</h2>
         <p>You scored: {scorePercentage}%</p>
+        {scorePercentage === 100 ? 
+        <div className="flex flex-col items-center">
+          <p>Congratulations! 🥳🎉</p>
+          <div className="flex justify-center">
+            <Image src="/images/owl-smiling.png" alt="Smiling Owl Image" width={400} height={400} />
+          </div>
+          <span id='rewardId' />
+        </div>
+         : 
         <>
         <div className="flex flex-row gap-8 mt-6">
           <Bar percentage={scorePercentage} color="green" />
@@ -24,7 +43,7 @@ const QuizzSubmission = (props: Props) => {
             <p>{score} Correct</p>
             <p>{totalQuestions - score} Incorrect</p>
           </div>
-        </>  
+        </>}  
       </main>
     </div>
   )
