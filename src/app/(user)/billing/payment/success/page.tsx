48734cf58fbdb94f1cf7cca0b87d400acc20dd50
Guy Ezra <guyez1@ac.sce.ac.il>
2024-08-05 16:14:55 +0300

    fixed payment success

--- src/app/(user)/billing/payment/success/page.tsx
+++ src/app/(user)/billing/payment/success/page.tsx
@@ -1,53 +1,18 @@
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import Link from 'next/link';
-import { createSubscription } from '@/app/actions/userSubscription'; 
-import { auth } from '@/auth'; 
-import { getUserSubscriptionId } from '@/app/actions/userSubscription'; 
-import { redirect } from 'next/navigation';
-import { getUser } from '@/auth/server';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import Link from "next/link";
 
-const SuccessPage = async () => {
-    try {
-        // Fetch session and user data
-        const session = await auth();
-        const regsession = await getUser();
-
-        const userId = session?.user?.id;
-        const regUserId = regsession?.id;
-
-        // Extract stripeCustomerID from session or regsession
-        const stripeCustomerID = await getUserSubscriptionId({
-            userId,
-            regUserId
-        });
-
-        // Check if stripeCustomerID is defined and is a string
-        if (typeof stripeCustomerID === 'string') {
-            await createSubscription({ stripeCustomerID });
-        } else {
-            console.warn("No valid stripeCustomerID found.");
-        }
-
-        // Optionally, redirect after successful operation
-        redirect('/dashboard');
-    } catch (error) {
-        console.error("Error in SuccessPage:", error);
-        // Handle error appropriately
-    }
-
+const page = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <Alert variant="default">
-                <AlertTitle className="mb-3 text-xl text-green-400">Success</AlertTitle>
-                <AlertDescription>
-                    Your account has been upgraded.
-                    <br />
-                    <Link href="/dashboard" className="underline">Go to dashboard </Link> 
-                    to generate more quizzes.
-                </AlertDescription>
-            </Alert>
-        </div>
-    );
+        <Alert variant="default">
+            <AlertTitle className="mb-3 text-xl text-green-400">Success</AlertTitle>
+            <AlertDescription>
+                Your account has been upgraded.
+                <br />
+                <Link href="/dashboard" className="underline">Go to dashboard </Link> 
+                to generate more quizzes.
+            </AlertDescription>
+        </Alert>
+    )    
 };
 
-export default SuccessPage;
+export default page;
